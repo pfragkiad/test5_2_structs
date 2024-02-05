82bf8ed3d4962eccec6a4338fb0a846f38e13ec7
Pavlos Fragkiadoulakis <pavlos.fragkiadoulakis@gmail.com>
2024-02-06 00:44:57 +0200

    Add implementation block.

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,18 @@ struct Rectangle {
     height: u32,
 }
 
+impl Rectangle { //implementation blocks
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
 fn main() {
     
-    let r1 : Rectangle = Rectangle{height:10,width:20};
+    let r1 : Rectangle = Rectangle{height:10, width:dbg!(20+10)};
 
-    println!("The rectangle is equal to {:#?}",r1);
+    println!("The rectangle is equal to {:#?} and its area equals {}",r1, r1.area());
+
+    dbg!(&r1);
+
 }
