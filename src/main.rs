bff45e1749e5dd4af6613b59f0cd72e50728171f
Pavlos Fragkiadoulakis <pavlos.fragkiadoulakis@gmail.com>
2024-02-06 01:52:05 +0200

    Add non-method implementations.

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,22 @@ struct Rectangle {
     height: u32,
 }
 
-impl Rectangle { //implementation blocks
-    fn area(&self) -> u32 {
+impl Rectangle { //implementation blocks (we can have one or multiple impl blocks)
+    fn area(&self) -> u32 { //associated functions
         self.width * self.height
     }
 
-    fn can_hold(&self, other: &Rectangle) -> bool 
+    fn can_hold(&self, other: &Rectangle) -> bool   //associated functions (METHOD)
     {
         self.width > other.width && self.height > other.height
+    }
+
+    fn square(size:u32) -> Self //associated function (NOT METHOD)
+    {
+        Self {
+            width : size,
+            height:size
+        }
     }
 }
 
@@ -22,5 +30,10 @@ fn main() {
     println!("The rectangle is equal to {:#?} and its area equals {}",r1, r1.area());
 
     dbg!(&r1);
+
+    //tried examples up until:
+    //https://doc.rust-lang.org/book/ch05-03-method-syntax.html
+    let r2 : Rectangle = Rectangle::square(100);
+    dbg!(&r2);
 
 }
